ae77458320acc0ad911039d0def3912c103df9dd
Euro20179 <sandollar2020@gmail.com>
2022-08-15 13:36:08 -0700

    version: 1.4.3.2_A

--- common.js
+++ common.js
@@ -6,7 +6,7 @@ const ADMINS = ["334538784043696130"]
 
 const LOGFILE = "log.txt"
 
-const VERSION = {major: 1, minor: 4, bug: 3, part: "2", beta: false, alpha: false}
+const VERSION = {major: 1, minor: 4, bug: 3, part: "2_B", beta: false, alpha: false}
 
 let WHITELIST = {}
 function reloadWhiteList(){
