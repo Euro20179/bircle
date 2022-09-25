997e574b77fc878f81b6d98060b8606d6652aba4
Euro20179 <sandollar2020@gmail.com>
2022-09-24 21:37:03 -0700

    version: 1.14.15

--- common.js
+++ common.js
@@ -8,7 +8,7 @@ const ADMINS = ["334538784043696130"]
 
 const LOGFILE = "log.txt"
 
-const VERSION = {major: 1, minor: 14, bug: 14, part: "", beta: false, alpha: false}
+const VERSION = {major: 1, minor: 14, bug: 15, part: "", beta: false, alpha: false}
 
 let USER_SETTINGS = {}
 
