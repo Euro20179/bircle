deea40577f66700fa8586dd867e6a87966bc354f
euro20179 <sandollar2020@gmail.com>
2023-07-06 19:38:45 -0700

    translate.docs: move language list to a -l option

--- src/commands/util_commands.ts
+++ src/commands/util_commands.ts
@@ -151,6 +151,10 @@ export default function*(CAT: CommandCategory): Generator<[string, Command | Com
 
     yield [
         'translate', ccmdV2(async function({ msg, args, opts, stdin }) {
+            if(opts.getBool("l", false)){
+
+                return crv(htmlRenderer.renderHTML("List of languages<br>af: afrikaans<br>sq: albanian<br>ar: Arabic<br>hy: Armenian<br>az: Azerbaijani<br>eu: Basque<br>be: Belarusian<br>bn: Bengali<br>bs: Bosnian<br>bg: Bulgarian<br>ca: Catalan<br>ceb: Cebuano<br>ny: Chichewa<br>zh-cn: Chinese Simplified<br>zh-tw: Chinese Traditional<br>co: Corsican<br>hr: Croatian<br>cs: Czech<br>da: Danish<br>nl: Dutch<br>en: English<br>eo: Esperanto<br>et: Estonian<br>tl: Filipino<br>fi: Finnish<br>fr: French<br>fy: Frisian<br>gl: Galician<br>ka: Georgian<br>de: German<br>el: Greek<br>gu: Gujarati<br>ht: Haitian Creole<br>ha: Hausa<br>haw: Hawaiian<br>iw: Hebrew<br>hi: Hindi<br>hmn: Hmong<br>hu: Hungarian<br>is: Icelandic<br>ig: Igbo<br>id: Indonesian<br>ga: Irish<br>it: Italian<br>ja: Japanese<br>jw: Javanese<br>kn: Kannada<br>kk: Kazakh<br>km: Khmer<br>ko: Korean<br>ku: Kurdish (Kurmanji)<br>ky: Kyrgyz<br>lo: Lao<br>la: Latin<br>lv: Latvian<br>lt: Lithuanian<br>lb: Luxembourgish<br>mk: Macedonian<br>mg: Malagasy<br>ms: Malay<br>ml: Malayalam<br>mt: Maltese<br>mi: Maori<br>mr: Marathi<br>mn: Mongolian<br>my: Myanmar (Burmese)<br>ne: Nepali<br>no: Norwegian<br>ps: Pashto<br>fa: Persian<br>pl: Polish<br>pt: Portuguese<br>ma: Punjabi<br>ro: Romanian<br>ru: Russian<br>sm: Samoan<br>gd: Scots Gaelic<br>sr: Serbian<br>st: Sesotho<br>sn: Shona<br>sd: Sindhi<br>si: Sinhala<br>sk: Slovak<br>sl: Slovenian<br>so: Somali<br>es: Spanish<br>su: Sudanese<br>sw: Swahili<br>sv: Swedish<br>tg: Tajik<br>ta: Tamil<br>te: Telugu<br>th: Thai<br>tr: Turkish<br>uk: Ukrainian<br>ur: Urdu<br>uz: Uzbek<br>vi: Vietnamese<br>cy: Welsh<br>xh: Xhosa<br>yi: Yiddish<br>yo: Yoruba<br>zu: Zulu"))
+            }
             let [to, ...t] = args
             let from = opts.getString("from", "auto")
             if (t.length && (t[0].length === 4 && t[0].startsWith("f:"))) {
@@ -186,7 +190,7 @@ export default function*(CAT: CommandCategory): Generator<[string, Command | Com
             })
             return crv(res.text)
         }, "translate from language a to b", {
-            docs: "List of languages<br>af: afrikaans<br>sq: albanian<br>ar: Arabic<br>hy: Armenian<br>az: Azerbaijani<br>eu: Basque<br>be: Belarusian<br>bn: Bengali<br>bs: Bosnian<br>bg: Bulgarian<br>ca: Catalan<br>ceb: Cebuano<br>ny: Chichewa<br>zh-cn: Chinese Simplified<br>zh-tw: Chinese Traditional<br>co: Corsican<br>hr: Croatian<br>cs: Czech<br>da: Danish<br>nl: Dutch<br>en: English<br>eo: Esperanto<br>et: Estonian<br>tl: Filipino<br>fi: Finnish<br>fr: French<br>fy: Frisian<br>gl: Galician<br>ka: Georgian<br>de: German<br>el: Greek<br>gu: Gujarati<br>ht: Haitian Creole<br>ha: Hausa<br>haw: Hawaiian<br>iw: Hebrew<br>hi: Hindi<br>hmn: Hmong<br>hu: Hungarian<br>is: Icelandic<br>ig: Igbo<br>id: Indonesian<br>ga: Irish<br>it: Italian<br>ja: Japanese<br>jw: Javanese<br>kn: Kannada<br>kk: Kazakh<br>km: Khmer<br>ko: Korean<br>ku: Kurdish (Kurmanji)<br>ky: Kyrgyz<br>lo: Lao<br>la: Latin<br>lv: Latvian<br>lt: Lithuanian<br>lb: Luxembourgish<br>mk: Macedonian<br>mg: Malagasy<br>ms: Malay<br>ml: Malayalam<br>mt: Maltese<br>mi: Maori<br>mr: Marathi<br>mn: Mongolian<br>my: Myanmar (Burmese)<br>ne: Nepali<br>no: Norwegian<br>ps: Pashto<br>fa: Persian<br>pl: Polish<br>pt: Portuguese<br>ma: Punjabi<br>ro: Romanian<br>ru: Russian<br>sm: Samoan<br>gd: Scots Gaelic<br>sr: Serbian<br>st: Sesotho<br>sn: Shona<br>sd: Sindhi<br>si: Sinhala<br>sk: Slovak<br>sl: Slovenian<br>so: Somali<br>es: Spanish<br>su: Sudanese<br>sw: Swahili<br>sv: Swedish<br>tg: Tajik<br>ta: Tamil<br>te: Telugu<br>th: Thai<br>tr: Turkish<br>uk: Ukrainian<br>ur: Urdu<br>uz: Uzbek<br>vi: Vietnamese<br>cy: Welsh<br>xh: Xhosa<br>yi: Yiddish<br>yo: Yoruba<br>zu: Zulu",
+            docs: "Use <code>translate -l</code> to list valid languages",
             accepts_stdin: "The text to translate",
             helpArguments: {
                 from: createHelpArgument("The language to translate from, put a for auto", false),
@@ -196,7 +200,8 @@ export default function*(CAT: CommandCategory): Generator<[string, Command | Com
             helpOptions: {
                 to: createHelpOption("The language to translate to"),
                 from: createHelpOption("The language to translate from"),
-                m: createHelpOption("Translate the message <b>m</b> messages up")
+                m: createHelpOption("Translate the message <b>m</b> messages up"),
+                l: createHelpOption("List valid languages")
             }
         })
     ]
