ed8efec947dd92b7017b6e68499b9fb2fcc4d405
euro20179 <sandollar2020@gmail.com>
2024-02-02 17:29:05 -0800

    version: 9.0.20

--- src/common.ts
+++ src/common.ts
@@ -3,7 +3,7 @@ import { User } from "discord.js";
 
 import { Client, GatewayIntentBits } from "discord.js"
 
-const VERSION = { major: 9, minor: 0, bug: 19, part: "", beta: false, alpha: false  }
+const VERSION = { major: 9, minor: 0, bug: 20, part: "", beta: false, alpha: false  }
 
 const client = new Client({ intents: [
     GatewayIntentBits.Guilds,
