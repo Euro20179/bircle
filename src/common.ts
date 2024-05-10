f27f171f25983dbf815430ffca1ed197df738029
euro20179 <sandollar2020@gmail.com>
2024-05-10 09:18:53 -0700

    version: 9.7.1

--- src/common.ts
+++ src/common.ts
@@ -11,7 +11,7 @@ import pets from './pets';
 
 import user_options from './user-options'
 
-const VERSION = { major: 9, minor: 7, bug: 0, part: "", beta: false, alpha: false  }
+const VERSION = { major: 9, minor: 7, bug: 1, part: "", beta: false, alpha: false  }
 
 const client = new Client({ intents: [
     GatewayIntentBits.Guilds,
