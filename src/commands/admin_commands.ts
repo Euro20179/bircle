aeb936d7bcb80e2297e08aa45ffad662976ab9e1
euro20179 <sandollar2020@gmail.com>
2025-08-28 23:27:43 -0700

    fix: reset-economy works on admin

--- src/commands/admin_commands.ts
+++ src/commands/admin_commands.ts
@@ -190,9 +190,6 @@ export default function*(): Generator<[string, CommandV2]> {
             })
         }, "Resets the economy", {
             permCheck: (m) => {
-                if(ADMINS.includes(m.author.id)) {
-                        return true
-                    }
                 let { total } = economy.economyLooseGrandTotal(false)
                 let necessary = amountParser.calculateAmountRelativeTo(total, `99%+100`)
                 return economy.playerLooseNetWorth(m.author.id) >= necessary || Number(hasItem(m.author.id, "reset economy")) > 0
